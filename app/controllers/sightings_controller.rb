a24385e9e220bcb3e2205b45a667912f23d84cad
Jonathan East <jonathan_east@ymail.com>
2020-12-07 15:23:38 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -14,3 +14,11 @@ class SightingsController < ApplicationController
     end
   end
 end
+
+# def show
+#   sighting = Sighting.find_by(id: params[:id])
+#   render json: sighting.to_json(:include => {
+#     :bird => {:only => [:name, :species]},
+#     :location => {:only => [:latitude, :longitude]}
+#   }, :except => [:updated_at])
+# end
